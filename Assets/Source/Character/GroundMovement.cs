1610488018f808d30f732e975f94af5e3affe7bd
Neil Petrick <vengantmjolnir@gmail.com>
2019-10-25 21:00:34 -0700

    Adding source folder and ground movement test scene/script

---
+++ Assets/Source/Character/GroundMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public float force = 10f;
+
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis(horizontalAxis);
+        float v = Input.GetAxis(verticalAxis);
+
+        if (h == 0 && v == 0)
+        {
+            return;
+        }
+
+        _rigidbody.AddForce(_transform.forward * v * force);
+    }
+}
